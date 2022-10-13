7a4fdf750acf3f585e268f9674e65c0500faea9d
Jaszmijee <jaszmije@gmail.com>
2022-10-13 11:12:00 +0200

    6.2 First unit test, ! see comment in Calculator class

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        int a = 12;
+        int b = 5;
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(a, b);
+
+        if (addResult == (a + b)) {
+            System.out.println("Testing add() method: - test OK");
+        } else {
+            System.out.println("Testing add() method: - Error");
+        }
+
+        int subtractResult = calculator.subtract(a, b);
+
+        if (subtractResult == (a - b)) {
+            System.out.println("Testing subtract() method: - test OK");
+        } else {
+            System.out.println("Testing subtract() method: - Error");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+    // The mistake in subtract method is done on purpose (for the sake of testing of testing :))
+    public int subtract(int a, int b){
+        return a + b;
+    }
+}
