8a8cf0c41d609f388f8364dbacaee6e9b9c2dab9
Jaszmijee <jaszmije@gmail.com>
2022-12-06 14:18:30 +0100

    13.4 Added examples of NamedQueries

---
+++ kodilla-hibernate/src/test/java/com/kodilla/kodilla/hibernate/manytomany/dao/EmployeeDaoTestSuite.java
@@ -0,0 +1,65 @@
+package com.kodilla.kodilla.hibernate.manytomany.dao;
+
+import com.kodilla.kodilla.hibernate.manytomany.Company;
+import com.kodilla.kodilla.hibernate.manytomany.Employee;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class EmployeeDaoTestSuite {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @Test
+    void testSaveManyToMany() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        //When
+        employeeDao.save(johnSmith);
+        int johnSmithId = johnSmith.getId();
+        employeeDao.save(stephanieClarckson);
+        int stephanieClarcksonId = stephanieClarckson.getId();
+        employeeDao.save(lindaKovalsky);
+        int lindaKovalskyId = lindaKovalsky.getId();
+
+        List<Employee> retrieveLastNamePositive = employeeDao.retrieveLastName("Smith");
+        List<Employee> retrieveLastNameNegative = employeeDao.retrieveLastName("Mat");
+
+        //Then
+        assertEquals(1, retrieveLastNamePositive.size());
+        assertEquals(0, retrieveLastNameNegative.size());
+
+        //CleanUp
+        try {
+            employeeDao.deleteById(johnSmithId);
+            employeeDao.deleteById(stephanieClarcksonId);
+            employeeDao.deleteById(lindaKovalskyId);
+        } catch (Exception e) {
+        }
+    }
+}
